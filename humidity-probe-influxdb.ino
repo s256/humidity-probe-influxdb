fba23d0ba492addd02e4a30d6626a5ffc2ebd45f
Sebastian Schneider <sebastian.schneider@boxine.de>
2020-08-05 23:16:18 +0200

    autoformat

--- humidity-probe-influxdb.ino
+++ humidity-probe-influxdb.ino
@@ -19,40 +19,44 @@
 #include <sstream>
 
 #include <esp_sleep.h>
-#include <esp_log.h>    // support for timestamps
+#include <esp_log.h> // support for timestamps
 
 /* custom.h defines the following constants:
  * - WIFI_SSID
  * - WIFI_PASSWORD
  * - INFLUXDB_REST_SERVICE_URL
  */
-#include "custom.h"     
+#include "custom.h"
 
 #define WIFI_RECONNECT_TRY_MAX 10
 #define WIFI_RECONNECT_DELAY_MS 500
 
 #define TIMESERVER "de.pool.ntp.org"
+#define SLEEP_TIME 60 * 1000       // time between measurements in ms
+#define DATA_TRANSFER_BATCH_SIZE 5 // transfer after this number of items have been collected
 
 // Define data record
-struct Measurement {
+struct Measurement
+{
   float temperature;
   float humidity;
   float pressure;
   time_t time;
 };
 
-QList<Measurement> queue;       // Queue
-HTTPClient http;                // HTTPClient user for transfering data to InfluxDB
-Adafruit_BME280 bme;            // I2C
-
+QList<Measurement> queue; // Queue
+HTTPClient http;          // HTTPClient user for transfering data to InfluxDB
+Adafruit_BME280 bme;      // I2C
 
 /**
  * Setup the humidity probe component.
  */
-void setup() {
-    Serial.begin(115200);
-    Serial.println(F("BME280 test"));
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println(F("BME280 test"));
 
+  // disable bluetooth to save power
   btStop();
 
   // default settings
@@ -61,19 +65,24 @@ void setup() {
   const char *ntpServer = "pool.ntp.org";
   // (you can also pass in a Wire library object like &Wire2)
   bool status = bme.begin(0x76);
-    bool status = bme.begin(0x76);  
-  bool status = bme.begin(0x76);
-    if (status) {
-        Serial.println("Detected BME280 sensor at 0x76.");
-    } else {
-        status = bme.begin(0x77);
-        if (status) {
-          Serial.println("Detected BME280 sensor at 0x77.");
-        } else {
-          Serial.println("ERROR: No BME280 sensor found. Please check the wiring!");
-          while(1);
-        }
+  if (status)
+  {
+    Serial.println("Detected BME280 sensor at 0x76.");
+  }
+  else
+  {
+    status = bme.begin(0x77);
+    if (status)
+    {
+      Serial.println("Detected BME280 sensor at 0x77.");
     }
+    else
+    {
+      Serial.println("ERROR: No BME280 sensor found. Please check the wiring!");
+      while (1)
+        ;
+    }
+  }
 
   Serial.println("Connecting to Wifi...");
   while (!connectWifi())
@@ -87,22 +96,27 @@ void setup() {
     delay(500);
   }
   Serial.println("Done...");
-    WiFi.mode(WIFI_OFF);
+  WiFi.mode(WIFI_OFF);
 }
 
 /**
  * Main program
  */
-void loop() { 
-    long now = esp_log_timestamp();
-    // obtain measurments from the sensor
-    struct Measurement m;
-    m.temperature = bme.readTemperature();
-    m.humidity = bme.readHumidity();
-    m.pressure = bme.readPressure() / 100.F;
-    time(&m.time);
+void loop()
+{
+  long now = esp_log_timestamp();
+  // obtain measurments from the sensor
+  struct Measurement m;
+  m.temperature = bme.readTemperature();
+  m.humidity = bme.readHumidity();
+  m.pressure = bme.readPressure() / 100.F;
+  time(&m.time);
 
-    queue.push_back(m);
+  queue.push_back(m);
+  Serial.println(queue.size());
+  if (queue.size() % DATA_TRANSFER_BATCH_SIZE == 0)
+  {
+    transferData();
   }
   Serial.println("Sleeping....");
   esp_sleep_enable_timer_wakeup(SLEEP_TIME * 1000 - (esp_log_timestamp() - now) * 1000);
@@ -113,15 +127,18 @@ void loop() {
 /** 
  * Transfers the data to the server.
  */
-void transferData() {
+void transferData()
+{
   // Connect to Wifi
-  if (!connectWifi()) {
+  if (!connectWifi())
+  {
     return;
   }
 
   // Transfer data
   Serial.println("Transfering data to InfluxDB");
-  while (queue.size() > 0 && transferBatch()) {
+  while (queue.size() > 0 && transferBatch())
+  {
     Serial.print(".");
   }
   Serial.println("\nCompleted :)");
@@ -136,37 +153,44 @@ void transferData() {
  * Returns:
  *  `true` if the transfer succeeds `else` otherwise.
  */
-boolean transferBatch() {
-    int endIndex = queue.size()-1;
-    if (endIndex < 0) {
-      return false;
-    }
-    int startIndex = endIndex - DATA_TRANSFER_BATCH_SIZE;
-    if (startIndex < 0) {
-      startIndex = 0;
-    }
-    
-    http.begin(INFLUXDB_REST_SERVICE_URL);
-    http.addHeader("Content-type", "text/plain");
-    std::ostringstream oss;
-    for (int i=endIndex; i>=startIndex; i--) {
-      struct Measurement m = queue[i];
-      oss << "bme280,sensor=bme280,host=" << WiFi.macAddress().c_str() << " temperature=" << m.temperature << ",humidity=" << m.humidity
-          << ",pressure="<< m.pressure << " " << m.time << "000000000\n"; // we need to add 9 zeros to the time, since InfluxDB expects ns.
-    }
-    int httpResponseCode = http.POST(oss.str().c_str());
-    http.end(); 
+boolean transferBatch()
+{
+  int endIndex = queue.size() - 1;
+  if (endIndex < 0)
+  {
+    return false;
+  }
+  int startIndex = endIndex - DATA_TRANSFER_BATCH_SIZE;
+  if (startIndex < 0)
+  {
+    startIndex = 0;
+  }
 
-    if (httpResponseCode != 204) {
-      Serial.println("Error on sending POST");
-      return false;
-    } 
-    
-    // clear the transferred items from the queue
-    for (int i=endIndex; i>=startIndex; i--) {
-      queue.clear(i);
-    }
-    return true;
+  http.begin(INFLUXDB_REST_SERVICE_URL);
+  http.addHeader("Content-type", "text/plain");
+  std::ostringstream oss;
+  for (int i = endIndex; i >= startIndex; i--)
+  {
+    struct Measurement m = queue[i];
+    oss << "bme280,sensor=bme280,host=" << WiFi.macAddress().c_str() << " temperature=" << m.temperature << ",humidity=" << m.humidity
+        << ",pressure=" << m.pressure << " " << m.time << "000000000\n"; // we need to add 9 zeros to the time, since InfluxDB expects ns.
+  }
+  int httpResponseCode = http.POST(oss.str().c_str());
+  http.end();
+
+  if (httpResponseCode != 204)
+  {
+    Serial.println("Error on sending POST");
+    Serial.print(httpResponseCode);
+    return false;
+  }
+
+  // clear the transferred items from the queue
+  for (int i = endIndex; i >= startIndex; i--)
+  {
+    queue.clear(i);
+  }
+  return true;
 }
 
 /**
@@ -176,11 +200,13 @@ boolean transferBatch() {
  * Returns:
  *   `true` if the connection succeeds `false` otherwise.
  */
-bool connectWifi() {
+bool connectWifi()
+{
   int count = 0;
   WiFi.mode(WIFI_AP);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED && count < WIFI_RECONNECT_TRY_MAX) {
+  while (WiFi.status() != WL_CONNECTED && count < WIFI_RECONNECT_TRY_MAX)
+  {
     delay(WIFI_RECONNECT_DELAY_MS);
     count++;
   }
